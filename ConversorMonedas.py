b4f71c71c6c4c1f80dc3f69eb05c24182e5472bd
iker alvarez <ikeralvarez777@gmail.com>
2020-08-17 16:30:31 +0200

    He optimizado el código utilizando floats en vez de variables

--- ConversorMonedas.py
+++ ConversorMonedas.py
@@ -1,12 +1,6 @@
-def conversor(tipode_moneda, valor, opcion):
+def conversor(tipode_moneda, cambio_dolares):
     valor = str(input("Ingresa un valor en dolares:   "))
-    if opcion == 1:
-        valor_dolar = float(3681.20)
-    elif opcion == 2:
-        valor_dolar = float(74.67)
-    elif opcion == 3:
-        valor_dolar = float(22.28)
-    valor_pesos = valor_dolar * float(valor)
+    valor_pesos = float(cambio_dolares) * float(valor)
     valor_pesos = str(round(valor_pesos, 2))
     print("el valor de "+ valor + "$ americanos" + " en "+ tipode_moneda +" es "+ valor_pesos +" $")
 
@@ -24,13 +18,13 @@ Elige una opción
 opcion = int(input(menu))
 
 if opcion == 1:
-    conversor("pesos colombianos", valor, opcion)
+    conversor("pesos colombianos", 3681.20)
 
 elif opcion == 2:
-    conversor("pesos argentino", valor, opcion)
+    conversor("pesos argentino", 3681.20)
 
 elif opcion == 3:
-    conversor("pesos mexicano", valor, opcion)
+    conversor("pesos mexicano", 3681.20)
 
 else:
     print("Opción no válida, por favor escoje entre una opción entre (1-2-3)")
---
+++ condicionales.py
@@ -0,0 +1,4 @@
+#Tipos de datos en Python
+#int -nº entero
+#float nº decimal
+#str es un string. Una cadena de texto
