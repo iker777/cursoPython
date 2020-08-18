4e6c0f34191f8d9c3826552f142eb4c359b6d5ed
iker alvarez <ikeralvarez777@gmail.com>
2020-08-18 07:22:27 +0200

    Añadidas BUENAS PRÁCTICAS al programa palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -7,15 +7,21 @@ def palindromo(palabra):
     else:
         return False
 
-menu = "1"
-while menu == "1":
 
-    palabra = str(input("Escribe una o varias palabras y te diremos si es Palíndromo \n"))
-    es_palindromo = palindromo(palabra)
+def run():
+    menu = "1"
+    while menu == "1":
 
-    if es_palindromo == True:
-        print("La palabra " + palabra + " Es palíndromo")
-    else:
-        print("La palabra " + palabra + " No es palíndromo")
+        palabra = str(input("Escribe una o varias palabras y te diremos si es Palíndromo \n"))
+        es_palindromo = palindromo(palabra)
 
-    menu = str(input("Si quieres volver a proponer una palabra, escribe 1. Si no, cualquier otra tecla \n"))
+        if es_palindromo == True:
+            print("La palabra " + palabra + " Es palíndromo")
+        else:
+            print("La palabra " + palabra + " No es palíndromo")
+
+        menu = str(input("Si quieres volver a proponer una palabra, escribe 1. Si no, cualquier otra tecla \n"))
+
+
+if __name__ == '__main__':
+    run()
