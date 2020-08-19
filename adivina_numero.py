f248b3f67e3cb037bb2603d66552859a3ad75b53
iker alvarez <ikeralvarez777@gmail.com>
2020-08-19 07:34:28 +0200

    adivina_numero.py v0.1 No hay vidas. Tienes infinito intentos

---
+++ adivina_numero.py
@@ -0,0 +1,39 @@
+import random
+
+
+# def es_correcto():
+
+
+#     if numero_usuario == numero_random:
+#         print("Felicidades TXO! Has acertado :)")
+#         return True
+
+#     elif numero_usuario > numero_random:
+#         numero_usuario = int(input("Tu número es más grande, vuelve a intentarlo:   "))
+#         print(numero_usuario)
+#         return False
+
+#     elif numero_usuario < numero_random:
+#         numero_usuario = int(input("Tu número es más pequeño, vuelve a intentarlo:   "))
+#         print(numero_usuario)
+#         return False
+
+
+def run():    
+    numero_random = random.randint(1, 100)
+    numero_usuario = int(input("Di un número del 1 al 100, a que no adivinas:   "))
+    print(numero_usuario, numero_random)
+
+    while numero_usuario != numero_random:
+        if numero_usuario > numero_random:
+            numero_usuario = int(input("Tu número es más grande, vuelve a intentarlo:   "))
+
+        elif numero_usuario < numero_random:
+            numero_usuario = int(input("Tu número es más pequeño, vuelve a intentarlo:   "))
+
+        else:
+            print("Felicidades TXO! Has acertado :)")
+
+            
+if __name__ == '__main__':
+    run()
---
+++ es_primo.py
@@ -0,0 +1,25 @@
+def es_primo(numero):
+    for i in range(numero+1, 1):
+        resto = numero % i
+        if resto == 0:
+            return True
+        if i == numero:
+            return False
+
+        
+def run():
+    numero = 0
+    while numero == 0:
+        numero = int(input("Escribe tu número aquí si quieres saber si es primo o no (no vale 0) \n"))
+        numero = round(numero)
+        numero_print = str(numero)
+    if numero < 0:
+        numero = -numero
+    if es_primo(numero) == True:
+        print(numero_print + " No es Primo")
+    else:
+        print(numero_print + " No es Primo")
+
+
+if __name__ == '__main__':
+    run()
