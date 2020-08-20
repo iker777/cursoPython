85560b3aede1e88529f31cc3bbea83d1ce7b2a94
iker alvarez <ikeralvarez777@gmail.com>
2020-08-20 07:45:15 +0200

    password_generation.py v0.2 code optimizated. I used the function randomize_list() to reduce the code.

--- password_generation.py
+++ password_generation.py
@@ -1,47 +1,32 @@
 import random
 
+
+#It gets a list of 5 characters
+def randomize_list(list):
+    counter = 0
+    character_list = [ ]
+
+    while counter != 5:
+        counter += 1
+        random_number = random.randrange(0, 9) 
+        character_list.append(list[random_number])
+
+    return character_list
+
 def generate_password():
+    #Random Character list
     letter_lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
     letter_upper = ["A", "B", 'C', 'D', 'E', 'F', 'G', 'H', 'I']
     number = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     special_character = ['!', '"', '·', '$', '%', '&', '/', '(', ')']
-    counter = 0
-    inner_lower = [ ]
-    inner_upper = [ ]
-    inner_number = [ ]
-    inner_character = [ ]
 
-    #It takes 5 lower letter list
-    while counter != 5:
-        counter += 1
-        random_number = random.randrange(0, 9) 
-        inner_lower.append(letter_lower[random_number])
-    print(inner_lower)
+    #I have 4 lists of random Characters
+    inner_lower = randomize_list(letter_lower)
+    inner_upper = randomize_list(letter_upper)
+    inner_number = randomize_list(number)
+    inner_character = randomize_list(special_character)
 
-    counter = 0
-
-    while counter != 5:
-        counter += 1
-        random_number = random.randrange(0, 9) 
-        inner_upper.append(letter_upper[random_number])
-    print(inner_upper)
-
-    counter = 0
-
-    while counter != 5:
-        counter += 1
-        random_number = random.randrange(0, 9) 
-        inner_number.append(number[random_number])
-    print(inner_number)
-
-    counter = 0
-
-    while counter != 5:
-        counter += 1
-        random_number = random.randrange(0, 9) 
-        inner_character.append(special_character[random_number])
-    print(inner_character)
-    
+    #It gets 5 lower letter list
     password_list = inner_lower + inner_upper + inner_number + inner_character
     password_inner = ""
     password_inner = password_inner.join(password_list)
@@ -49,11 +34,9 @@ def generate_password():
     return password_inner
 
 
-
-
 def run():
     password = generate_password()
-    print(password)
+    print("Tu contraseña secreta es:\n" + password)
 
 
 if __name__ == '__main__':
