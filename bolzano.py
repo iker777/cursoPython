d9941f557e9088eef59d47fef84008de08c4a1bc
iker alvarez <ikeralvarez777@gmail.com>
2020-08-27 22:37:15 +0200

    bolzano.py v0.2 da como resultado 2 raíces

--- bolzano.py
+++ bolzano.py
@@ -1,52 +1,63 @@
 import unittest
 import math
 
-
-def bolzano():
-    print("Esta va a ser tu ecuación de tercer grado: Ax^2 + Bx + C + D = 0. Introduce los factores de tu ecuación")
-    A = float(input("A: "))
-    B = float(input("B: "))
-    C = float(input("C: "))
-    D = float(input("D: "))
-    # A = float(1)
-    # B = float(0)
-    # C = float(-5)
-    # D = float(0)
-
-    a = float(0.1)
-    fa = A*a**2 + B*a + C + D
-    b = float(100)
-    b_max = b
-    b_min = a
-
+def finding_solution(var_A, var_B, var_C, var_D, limite_min, limite_max):
+    a = limite_min
+    b = limite_max
+    fa = var_A*a**2 + var_B*a + var_C + var_D
+    
     for i in range(100):
-        fb = A*b**2 + B*b + C + D
+        fb = var_A*b**2 + var_B*b + var_C + var_D
         bolzano = fa * fb
 
         if fb < 0.001 and fb > -0.001:
-            return round(b, 4)
+            return round(b, 3)
 
         elif bolzano < 0:
             #Hay una raíz entre a y b
-            b_max = b
+            limite_max = b
             #print('b más PEQUEÑA', b_max, b_min, b)
-            b = (b_max + b_min)/2
+            b = (limite_max + limite_min)/2
             continue
 
         elif bolzano > 0:
             #No hay una raíz entre a y b
-            b_min = b
+            limite_min = b
             #print('b más GRANDE', b_max, b_min, b)
-            b = (b_max + b_min)/2
+            b = (limite_max + limite_min)/2
             continue
         
     return "No tiene respuesta"
+
+def bolzano():
+    print("Esta va a ser tu ecuación de tercer grado: Ax^2 + Bx + C + D = 0. Introduce los factores de tu ecuación")
+    # A = float(input("A: "))
+    # B = float(input("B: "))
+    # C = float(input("C: "))
+    # D = float(input("D: "))
+    A = float(1)
+    B = float(0)
+    C = float(-5)
+    D = float(0)
+
+    sol1_min = float(0.1)
+    sol1_max = float(1000)
+    sol1 = finding_solution(A, B, C, D, sol1_min, sol1_max)
+
+    sol2_min = float(-1000)
+    sol2_max = sol1 - 0.5
+    sol2 = finding_solution(A, B, C, D, sol2_min, sol2_max)
+
+    return sol1, sol2
+
             
 class TestBolzano(unittest.TestCase):
     def test_run(self):
-        solution = bolzano()
-        print("la respuesta de x es: " + str(solution))
-        self.assertEqual(solution, round(math.sqrt(5), 4))
+        solution1, solution2 = bolzano()
+        print("la respuesta de x es: " + str(solution1) + " " + str(solution2))
+        self.assertEqual(solution1, round(math.sqrt(5), 3))
+
+        self.assertEqual(solution2, -1*round(math.sqrt(5), 3))
 
 if __name__ == '__main__':
     unittest.main()
