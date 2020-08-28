410550cdef313c09fa6d09eb53149be88438fc8f
iker alvarez <ikeralvarez777@gmail.com>
2020-08-28 06:54:23 +0200

    Caché de Python creado por usar bolzano.py como función de math_algorithm.py

--- math_algorithm.py
+++ math_algorithm.py
@@ -1,4 +1,5 @@
 import bolzano
+import math
 
 def equation():
     print("Esta va a ser tu ecuación de tercer grado: Ax^2 + Bx + C = D. Introduce los factores de tu ecuación")
@@ -26,8 +27,10 @@ def exhaustive_enumeration(var1, var2, var3, var4):
             print("No hemos encontrado respuesta con este método, vuelve a intentarlo")
 
 
-def binary_search(var1, var2, var3, var4):
-    pass
+def equation_dbh(var1, var2, var3, var4):
+    sol1 = round((-var2 + math.sqrt(var2**2 - 4*var1 * (var3 + var4))) / 2*var1, 3)
+    sol2 = round((-var2 - math.sqrt(var2**2 - 4*var1 * (var3 + var4))) / 2*var1, 3)
+    return sol1, sol2
 
 
 def run():
@@ -38,7 +41,7 @@ def run():
                                     
                                     1. Enumeración exhaustiva
                                     2. Bolzano
-                                    3. Búsqueda binaria   
+                                    3. Ecuación típica de DBH   
                                     
                                     Elige:  """))
 
@@ -60,9 +63,10 @@ def run():
         
         elif option == 3:
             a,b,c,d = equation()
-            answer = binary_search(a, b, c, d)
+            solution1, solution2 = equation_dbh(a, b, c, d)
+            print("la respuesta de x son --> solución1: " + str(solution1) + "\n                      --> y solución2: " + str(solution2))
 
-            if answer != None:
+            if solution1 != str or solution2 != str:
                 break  
 
 
