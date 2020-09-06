8439fd36c4ffb0c2924e852b45f48ed63b5cd78b
iker alvarez <ikeralvarez777@gmail.com>
2020-09-06 09:58:26 +0200

    complejidad_algoritmica.py tiempo en ejecutar un programa

---
+++ complejidad_algoritmica.py
@@ -0,0 +1,29 @@
+import time
+def factorial(n):
+    respuesta = 1
+
+    while n > 1:
+        respuesta *= n
+        n -= 1
+
+    return respuesta
+
+
+def factorial_r(n):
+    if n == 1:
+        return 1
+
+    return n * factorial(n -1)
+
+
+if __name__ == '__main__':
+    comienzo = time.time()
+    print(factorial(10))
+    final = time.time()
+    print(final - comienzo)
+    
+    comienzo = time.time()
+    print(factorial_r(10))
+    final = time.time()
+    print(final - comienzo)
+    
